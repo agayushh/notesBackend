5c81ee1c0d2976e89ba396405a71b150cfab4aff
agayushh <agayush088@gmail.com>
2026-02-20 16:38:58 +0530

    handling error and success messages

--- src/middleware/logSecurity.ts
+++ src/middleware/logSecurity.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import { extractRequestMetadata } from "../utils/getMetaData.js";
+import { timeStamp } from "console";
 export const requestLogger = (
   req: Request,
   res: Response,
@@ -12,14 +13,33 @@ export const requestLogger = (
   res.on("finish", () => {
     const endTime = Date.now();
     const duration = endTime - startTime;
-    console.log({
+
+    const logData = {
       timeStamp: new Date().toISOString(),
       method: req.method,
       path: req.path,
       status: res.statusCode,
       duration: `${duration} ms`,
       ...metaData,
-    });
+    };
+    if (res.statusCode.toString() >= "500") {
+      console.log({
+        ...logData,
+        level: "WARN",
+        error: res.locals.error,
+      });
+    } else if (res.statusCode.toString() >= "400") {
+      console.log({
+        ...metaData,
+        level: "SERVER ERROR",
+        error: res.locals.error,
+      });
+    } else {
+      console.log({
+        ...metaData,
+        level: "INFO",
+      });
+    }
   });
 
   return next();
