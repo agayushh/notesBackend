cb5e1cf33542dfc285a6cd2ca37b483844e67d79
agayushh <agayush088@gmail.com>
2026-05-05 20:32:18 +0530

    feat: Add JWT authentication to login and registration processes

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -1,6 +1,7 @@
 import { type Request, type Response } from "express";
 import prisma from "../../config/prismaInstance.js";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -15,11 +16,45 @@ export const login = async (req: Request, res: Response) => {
     },
   });
 
-  if (!user || user.password !== password) {
+  const correctPassword = await bcrypt.compare(password, user?.password!);
+
+  if (!user || !correctPassword) {
     return res.status(409).json({ message: "User not found kindly register" });
   }
 
-  res.status(200).json({
-    message: "Success",
+  const accessToken = jwt.sign(
+    {
+      userId: user.id,
+    },
+    process.env.ACCESS_TOKEN_SECRET!,
+    { expiresIn: "15m" },
+  );
+  const refreshToken = jwt.sign(
+    {
+      userId: user.id,
+    },
+    process.env.REFRESH_TOKEN_SECRET!,
+    { expiresIn: "7d" },
+  );
+  await prisma.user.update({
+    where: { id: user.id },
+    data: { refreshToken },
   });
+
+  const { password: _, ...safeUser } = user;
+
+  return res
+    .cookie("accessToken", accessToken, {
+      httpOnly: true,
+      sameSite: "strict",
+    })
+    .cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      sameSite: "strict",
+    })
+    .status(201)
+    .json({
+      message: "User created successfully",
+      user: safeUser,
+    });
 };
--- src/controllers/auth/register.ts
+++ src/controllers/auth/register.ts
@@ -1,6 +1,7 @@
 import prisma from "../../config/prismaInstance.js";
 import type { Request, Response } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -9,23 +10,57 @@ export const register = async (req: Request, res: Response) => {
     return res.status(400).json({ message: "need to enter all credentials" });
   }
 
-  const user = prisma.user.findUnique({
+  const user = await prisma.user.findUnique({
     where: {
       email,
     },
   });
 
-  const hashpassword = await bcrypt.hash(password, 10)
+  if (user) {
+    return res
+      .status(400)
+      .json({ message: "you already have an account kindly login" });
+  }
 
-  if (!user) {
-     const createdUser = await prisma.user.create({
-       data: {
-         email,
-         password: hashpassword
-      }, 
+  const hashpassword = await bcrypt.hash(password, 10);
+  const createdUser = await prisma.user.create({
+    data: {
+      email,
+      password: hashpassword,
+    },
+  });
+  const accessToken = jwt.sign(
+    {
+      userId: createdUser.id,
+    },
+    process.env.ACCESS_TOKEN_SECRET!,
+    { expiresIn: "15m" },
+  );
+  const refreshToken = jwt.sign(
+    {
+      userId: createdUser.id,
+    },
+    process.env.REFRESH_TOKEN_SECRET!,
+    { expiresIn: "7d" },
+  );
+  await prisma.user.update({
+    where: { id: createdUser.id },
+    data: { refreshToken },
+  });
+  const { password: _, ...safeUser } = createdUser;
+
+  return res
+    .cookie("accessToken", accessToken, {
+      httpOnly: true,
+      sameSite: "strict",
+    })
+    .cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      sameSite: "strict",
+    })
+    .status(201)
+    .json({
+      message: "User created successfully",
+      user: safeUser,
     });
-  }
-  else{
-    return res.status(400).json({message: "you already have an account kindly login"})
-  }
 };
